1e4c52d84ef5db55b27a65c3529eb1bacd21d4fe
Jonathan Grimes <jsg2021@gmail.com>
2020-11-20 12:40:44 -0600

    drop karma, run tests with jest

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,2 @@
 'use strict';
-const base = require('@nti/lib-scripts/config/babel.config');
-
-module.exports = function (api) {
-	return {
-		...base(api),
-		babelrc: false,
-		presets: [
-			['@babel/preset-env', {
-				targets: {
-					browsers: [
-						'> 1% in US',
-						'last 2 versions',
-						'not dead'
-					]
-				}
-			}],
-			'@babel/preset-flow'
-		]
-	};
-};
+module.exports = require('@nti/lib-scripts/config/babel.config');
--- test/main.js
+++
@@ -1,8 +0,0 @@
-require('@babel/polyfill');
-if (typeof localStorage !== 'undefined') {
-	localStorage.setItem('debug', 'off');
-}
-
-const importAll = x => x.keys().forEach(x);
-
-importAll(require.context('../src/', true, /\.js$/));
